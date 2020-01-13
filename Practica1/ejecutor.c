aa52708cfccfb3bc87b42632b7da2da6de94c75a
ggzor <30713864+ggzor@users.noreply.github.com>
2020-01-13 09:38:10 -0600

    Terminando ejecutor de procesos

---
+++ Practica1/ejecutor.c
@@ -0,0 +1,44 @@
+// Para que no se incluya otro main de alguna interfaz
+#define _MAIN_DEFINIDO_
+
+#include "listas.h"
+#include "planeador.h"
+#include "utilerias.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <stdio.h>
+
+#define SALIDA "salida.txt"
+
+int main() {
+  int fd;
+  Nodo primero;
+  Nodo *cabeza = &primero;
+
+  // Llevar a cabo las planeaciones
+  int tiempoTotal = operar(cabeza);
+
+  // Ignorar primer nodo centinela
+  cabeza = cabeza->siguiente;
+
+  if ((fd = creat(SALIDA, 0666)) < 0)
+    exitError("No se pudo crear el archivo de salida.");
+
+  // Información de los procesos
+  fprintf(fd, "Tiempo total: %d\n", tiempoTotal);
+  fprintf(fd, "PID Tiempo Prioridad Espera Total\n");
+  while (cabeza != NULL) {
+    fprintf(fd, "\n%3d %6d %9d %6d %5d",
+              cabeza->proceso.nombre,
+              cabeza->proceso.tiempo,
+              cabeza->proceso.prioridad,
+              cabeza->proceso.espera,
+              cabeza->proceso.tiempo + cabeza->proceso.espera);
+
+    cabeza = cabeza->siguiente;
+  }
+
+  close(fd);
+}
