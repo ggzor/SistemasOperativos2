93c35fa5e05f3c592d2d1f389bc5606cc19b5d86
ggzor <30713864+ggzor@users.noreply.github.com>
2020-01-12 23:12:33 -0600

    Agregando definiciones extra para el manejo de listas

--- Practica1/listas.h
+++ Practica1/listas.h
@@ -2,7 +2,10 @@
 #define __LISTAS_H__
 
 typedef struct Nodo {
-  
+  Nodo *siguiente;
 } Nodo;
+
+// Agrega un proceso al final de la lista
+void agregar(Nodo *lista, Proceso *proceso);
 
 #endif // __LISTAS_H__
