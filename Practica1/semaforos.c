b4bc12fd036bc0c91cc8b59d86768b0f077328bd
ggzor <30713864+ggzor@users.noreply.github.com>
2020-01-16 15:21:13 -0600

    Corrigiendo implementación de incremento y decremento en semáforos

--- Practica1/semaforos.c
+++ Practica1/semaforos.c
@@ -1,20 +1,22 @@
 // Propias 
 #include "semaforos.h"
 
-struct sembuf decremento = { 0, -1, SEM_UNDO}; 
-struct sembuf incremento = { 0, +1, SEM_UNDO}; 
+struct sembuf decremento = { .sem_num = 0, .sem_op = -1, .sem_flg = 0 }; 
+struct sembuf incremento = { .sem_num = 0, .sem_op = +1, .sem_flg = 0 }; 
 
 int semIncrementar(int semaforo, int indice){
-    return semop(semaforo, &incremento, 1);
+  incremento.sem_num = indice;
+  return semop(semaforo, &incremento, 1);
 }
 
 int semDecrementar(int semaforo, int indice){
-    return semop(semaforo, &decremento, 1);
+  decremento.sem_num = indice;
+  return semop(semaforo, &decremento, 1);
 }
 
 int semValor(int semaforo, int indice){
-    union semun arg;
-    return semctl(semaforo, indice,  GETVAL, arg); 
+  union semun arg;
+  return semctl(semaforo, indice,  GETVAL, arg);
 }
 
 int abrirSemaforo(int key){
