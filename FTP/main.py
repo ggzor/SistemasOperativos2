4ce780810ffbfa00bf8c67ce1b9d9079a4ec363e
ggzor <30713864+ggzor@users.noreply.github.com>
2020-04-28 08:11:59 -0500

    Moviendo comandos locales a su propio archivo

--- FTP/main.py
+++ FTP/main.py
@@ -3,6 +3,7 @@ import threading
 from functools import singledispatch
 
 import comunicacion_async
+from comandos_locales import ejecutar_comando, imprimir_resultado
 from comandos_remotos import ejecutar_remoto
 from lectura_comandos import (ComandoInvalido, ComandoSalir, leer_comando,
                               leer_configuracion)
@@ -24,54 +25,6 @@ async def ejecutar_servidor(config):
 
 def preparar_servidor(config):
     asyncio.run(ejecutar_servidor(config))
-
-
-async def ejecutar_comando(comando, config):
-    destinos, comando = comando
-    try:
-        reader, writer = await asyncio.open_connection(destinos[0], config["port"])
-        return await procesar_comando(comando, reader, writer)
-    except ConnectionRefusedError:
-        return ErrorConexion(destinos[0])
-
-
-@singledispatch
-async def procesar_comando(comando, reader, writer):
-    await comunicacion_async.send_packet(writer, comando)
-    return await comunicacion_async.recv_packet(reader)
-
-
-@procesar_comando.register
-async def _(comando: Descargar, reader, writer):
-    await comunicacion_async.send_packet(writer, comando)
-    respuesta = await comunicacion_async.recv_packet(reader)
-
-    if isinstance(respuesta, Continuar):
-        print(
-            f"Descargando archivo {respuesta.datos.nombre} ({respuesta.datos.tamano} bytes)..."
-        )
-        with open(respuesta.datos.nombre, "wb") as f:
-            f.write(await reader.readexactly(respuesta.datos.tamano))
-        print("Archivo descargado.")
-    else:
-        return respuesta
-
-
-@procesar_comando.register
-async def _(comando: Subir, reader, writer):
-    await comunicacion_async.send_packet(writer, comando)
-    respuesta = await comunicacion_async.recv_packet(reader)
-
-    if isinstance(respuesta, Continuar):
-        print(
-            f"Subiendo archivo {comando.archivo.nombre} ({comando.archivo.tamano} bytes)..."
-        )
-        with open(comando.archivo.nombre, "rb") as f:
-            writer.write(f.read())
-            await writer.drain()
-        print("Archivo subido.")
-    else:
-        return respuesta
 
 
 async def evaluar(comando, config):
@@ -109,31 +62,6 @@ async def main():
             pass
     else:
         await evaluar(config["comando"], config)
-
-
-@singledispatch
-def imprimir_resultado(resultado, config):
-    if resultado != None:
-        if isinstance(resultado, NoEncontrado):
-            print(f"No se encontró el archivo: {resultado.nombre}")
-        elif isinstance(resultado, NoAccesible):
-            print(f"No se pudo acceder al archivo: {resultado.nombre}")
-        elif isinstance(resultado, RutaInvalida):
-            print(f"La ruta dada no es válida: {resultado.nombre}")
-        elif isinstance(resultado, SobrescrituraInvalida):
-            print(
-                f"No se proporcionó la opción de sobrescribir el archivo: {resultado.nombre}"
-            )
-        elif isinstance(resultado, ErrorConexion):
-            print(f"No se pudo conectar: {resultado.ip}")
-        else:
-            raise NotImplementedError("Respuesta inesperada", resultado)
-
-
-@imprimir_resultado.register
-def _(resultado: ListaArchivos, config):
-    print(f"Existen {len(resultado.archivos)} archivos:")
-    print("\n".join(f"  - {x.nombre}" for x in resultado.archivos))
 
 
 def obtener_direccion():
